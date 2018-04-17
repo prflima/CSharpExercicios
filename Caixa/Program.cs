32f8273e3d6f743a2849e96ed042b558fd06ef23
Paulo Ricardo <paulo_tolkien@outlook.com>
2018-04-17 12:22:55 -0300

    AulasEdilson

---
+++ Caixa/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Caixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto;
+            double preco;
+            bool formaDePagamento;
+            double resultado;
+            double precoFinal;
+
+            Console.WriteLine("Digite o nome do produto:");
+            produto = Console.ReadLine();
+            
+            Console.WriteLine("Digite o preço do produto:");
+            preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("O Pagamento será em dinheiro?");
+            formaDePagamento = bool.Parse(Console.ReadLine());
+
+            if(formaDePagamento==true){
+                resultado = preco * 10/100;
+                precoFinal = preco-resultado;
+            }
+            else {
+                resultado = preco * 5/100;
+                precoFinal = preco+resultado;
+            }
+
+            StreamWriter arquivo = new StreamWriter("dados.txt");
+            arquivo.WriteLine("O produto escolhido foi: "+produto);
+            arquivo.WriteLine("O preço á vista do produto é "+precoFinal);
+            arquivo.WriteLine("O valor de desconto ou acréscimo é "+resultado);
+            arquivo.WriteLine("O preço final do produto é "+precoFinal);
+
+            Console.WriteLine("O produto escolhido foi: "+produto);
+            Console.WriteLine("O preço á vista do produto é "+precoFinal);
+            Console.WriteLine("O valor de desconto ou acréscimo é "+resultado);
+            Console.WriteLine("O preço final do produto é "+precoFinal);
+
+
+        }
+    }
+}
---
+++ FirstProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            double n1;
+            double n2;
+            double soma, subtrair, multiplicar, dividir, resto;
+
+            n1=50;
+            n2=20;
+
+            soma = n1+n2;
+            subtrair = n1-n2;
+            multiplicar = n1*n2;
+            dividir = n1/n2;
+            resto = n1%n2;
+            Console.WriteLine("A Soma de"+n1+" e "+n2+"é igual a"+soma);
+            Console.WriteLine("A Subtração de"+n1+" e "+n2+"é igual a"+subtrair);
+            Console.WriteLine("A Multiplicação de"+n1+"e"+n2+"é igual a"+multiplicar);
+            Console.WriteLine("A divisão de"+n1+"e"+n2+"é igual a"+dividir);
+            Console.WriteLine("O resto da divisão entre "+n1+ "e" +n2+"é igual a" +resto);
+        }
+    }
+}
